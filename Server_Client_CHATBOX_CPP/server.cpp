738c46850c3ecd6449d34f1eccce8c415ed75867
Bernardo Lorenzini <87033537+bernardolorenzini@users.noreply.github.com>
2023-08-16 15:57:38 -0300

    Update server.cpp
    client and server updated to use an window to change messages.
    
    form and button added

--- Server_Client_CHATBOX_CPP/server.cpp
+++ Server_Client_CHATBOX_CPP/server.cpp
@@ -4,8 +4,14 @@
 #include <netinet/in.h>
 #include <thread>
 #include <unistd.h>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QTextEdit>
+#include <QtWidgets/QPushButton>
 
-void receiveMessages(int clientSocket) {
+void receiveMessages(int clientSocket, QTextEdit *messageTextEdit) {
     char buffer[1024];
     while (true) {
         memset(buffer, 0, sizeof(buffer));
@@ -14,11 +20,31 @@ void receiveMessages(int clientSocket) {
             std::cerr << "Client disconnected." << std::endl;
             break;
         }
-        std::cout << "Client: " << buffer << std::endl;
+        QString receivedMessage = QString::fromUtf8(buffer);
+        messageTextEdit->append("Client: " + receivedMessage);
     }
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    QMainWindow window;
+    window.setWindowTitle("Server GUI");
+
+    QWidget *centralWidget = new QWidget(&window);
+    window.setCentralWidget(centralWidget);
+
+    QVBoxLayout *layout = new QVBoxLayout(centralWidget);
+
+    QTextEdit *messageTextEdit = new QTextEdit(centralWidget);
+    layout->addWidget(messageTextEdit, 0, Qt::AlignCenter);
+
+    QHBoxLayout *buttonLayout = new QHBoxLayout;
+    layout->addLayout(buttonLayout);
+
+    QPushButton *sendButton = new QPushButton("Send", centralWidget);
+    buttonLayout->addWidget(sendButton);
+
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in serverAddr;
@@ -34,19 +60,20 @@ int main() {
     int clientSocket = accept(serverSocket, nullptr, nullptr);
     std::cout << "Client connected." << std::endl;
 
-    std::thread receiveThread(receiveMessages, clientSocket);
+    std::thread receiveThread(receiveMessages, clientSocket, messageTextEdit);
 
-    char message[1024];
-    while (true) {
-        memset(message, 0, sizeof(message));
-        std::cout << "You: ";
-        std::cin.getline(message, sizeof(message));
-        send(clientSocket, message, sizeof(message), 0);
-    }
+    QObject::connect(sendButton, &QPushButton::clicked, [&]() {
+        QString message = messageTextEdit->toPlainText();
+        send(clientSocket, message.toUtf8().constData(), message.length(), 0);
+    });
+
+    window.show();
+
+    int result = app.exec();
 
     receiveThread.join();
     close(clientSocket);
     close(serverSocket);
 
-    return 0;
+    return result;
 }
